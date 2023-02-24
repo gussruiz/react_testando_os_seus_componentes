ce679f4dc31d4d2f4c554b216570d571068579c1
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-23 21:56:11 -0300

    participants list first tests

---
+++ src/componentes/ListaParticipantes.tsx
@@ -0,0 +1,12 @@
+import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes";
+
+export const ListaParticipantes = () => {
+  const participantes: string[] = useListaDeParticipantes()
+  return (
+    <ul>
+      {participantes.map((participante) => (
+        <li key={participante}>{participante}</li>
+      ))}
+    </ul>
+  );
+};
